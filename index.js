6660134585303f6a2600a72263f0b8fd63183f22
Alberto Coscia <alberto@albertocoscia.me>
2016-04-24 00:14:09 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = {};
+var Bot = module.exports.Bot = require('./lib/Bot.js'),
+    User = module.exports.User = require('./lib/User.js'),
+    Chat = module.exports.Chat = require('./lib/Chat.js'),
+    Message = module.exports.Message = require('./lib/Message.js'),
+    PhotoSize = module.exports.PhotoSize = require('./lib/PhotoSize.js'),
+    File = module.exports.File = require('./lib/File.js'),
+    InlineQuery = module.exports.InlineQuery = require('./lib/InlineQuery.js'),
+    ChosenInlineResult = module.exports.ChosenInlineResult = require('./lib/ChosenInlineResult.js'),
+    InlineQueryResultArticle = module.exports.InlineQueryResultArticle = require('./lib/InlineQueryResultArticle.js'),
+    InlineQueryResultLocation = module.exports.InlineQueryResultLocation = require('./lib/InlineQueryResultLocation.js');
